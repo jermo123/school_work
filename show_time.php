b457e0b94ac10618c9747fb77dbfed5ddd0e68fe
jermo123 <jmobley42@gmail.com>
2015-10-08 14:59:46 -0700

    Update show_time.php
    cleaned

--- show_time.php
+++ show_time.php
@@ -10,28 +10,27 @@
  * @author         Jeromy Mobley 
  * @version        1.0
  -->
-<!-- Debugging -->
-<?php
+ <?php
+ 
+// Debugging
 ini_set( 'display_errors', 1 );
 error_reporting( E_ALL | E_STRICT );
-?>
-<!-- Request -->
-<?php
-$input = $_REQUEST['input'];
-?>
-<!-- Static Numbers -->
-<?php 
+
+//Trim, Strip Tags, Request
+$total_seconds = trim(strip_tags($_REQUEST['input']));
+
+//Static Numbers
 $days = 86400;
 $hours = 3600;
 $minutes = 60;
+
+//Calculations for hr/day/min/sec
+$daysOut = floor(($total_seconds / $days) % $days);
+$hoursOut = floor(($total_seconds / $hours) % $hours);
+$minutesOut = floor(($total_seconds / $minutes) % $minutes);
+$secsOut = $total_seconds % $minutes;
 ?>
-<!-- Calculations for hr/day/min/sec -->
-<?php
-$daysOut = floor(($input / $days) % $days);
-$hoursOut = floor(($input / $hours) % $hours);
-$minutesOut = floor(($input / $minutes) % $minutes);
-$secsOut = $input % $minutes;
-?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -40,15 +39,18 @@ $secsOut = $input % $minutes;
 </head>
 <body> 
 <?php
- if ($input<0){
+//Negative number checker
+ if ($total_seconds<0){
       echo "Negative number detected. Please try again.";
     }
+//Output if not negative
     else {
-      echo "$input seconds = $daysOut days, $hoursOut hours, $minutesOut
+      echo "$total_seconds seconds = $daysOut days, $hoursOut hours, $minutesOut
        minutes, and $secsOut seconds.";
     }
 ?>
 <br /><br />
+<!-- Back button -->
 <button onclick="history.go(-1);">Convert again!</button>
 </body>
 </html>
